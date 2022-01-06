7402f26fe10fa5eec9ab3e7e52fe8334ad7c50a5
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-01-06 02:58:35 -0300

    exercise

--- Exercicios_Extra/src/com/company/Dado.java
+++ Exercicios_Extra/src/com/company/Dado.java
@@ -1,4 +1,30 @@
 package com.company;
 
+import java.util.*;
+
 public class Dado {
+    /*
+    Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+    Depois, mostre quantas vezes cada valor foi conseguido.
+ */
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 1; i <= 100; i++){
+            numeros.add(random.nextInt(1,7));
+        }
+
+        Map<Integer, Integer> lacamentos = new HashMap<>();
+        for (Integer numero: numeros) {
+
+            if (!lacamentos.containsKey(numero)){
+                lacamentos.put(numero, 1);
+
+            }else{
+                lacamentos.put(numero, lacamentos.get(numero) + 1);
+            }
+        }
+
+        System.out.println(lacamentos);
+    }
 }
